c345b7c0104c43da56f595b4549e853002824d19
Henrique Leote <henriqueleote@outlook.com>
2023-06-02 20:38:09 +0200

    Gettimg products for multiple pages

--- WortenChannelBot.py
+++ WortenChannelBot.py
@@ -25,16 +25,16 @@ channel_id = "-1001921638321"
 def load_queries():
     global queries
     try:
-        with open(DATA_FILE, 'r') as file:
-            data = file.read()
-            file = open('queries.txt', 'r')
-            lines = file.readlines()
-            count = 0
-            for line in lines:
-                count += 1
-                queries.append(line.strip())
+        file = open('queries.txt', 'r')
+        lines = file.readlines()
+        count = 0
+        for line in lines:
+            count += 1
+            queries.append(line.strip())
+
     except FileNotFoundError:
         queries = []
+        print('not found')
 
 
 # Load products from the JSON file
@@ -82,55 +82,52 @@ def runWebDriver(link):
 
 def getData(query, soup, bot):
 
-    p_element = soup.find('p', {'class': 'filter-and-sortblock__product-count'})
+    # <ul data-v-04484ba1="" class="listing-content__list listing-content__list--grid"> i want to get this element with soup
+    product_group = soup.select_one('.listing-content__list')
 
-    if p_element is not None:
-        span_element = p_element.find('span')
+    if product_group is not None:
 
-        product_count = span_element.contents[0]
+        # Find all li elements inside ul_element, excluding those with hidden="hidden"
+        product_list = product_group.find_all('li', attrs={'hidden': None})
 
-        if int(product_count) > 0:
+        # Process the li elements as needed
+        for li in product_list:
+            # Find product HTML Elements
+            product_name_span = li.findAll('span', {'class': 'produc-card__name__link'})
+            product_link_anchor = li.findAll('a')
+            product_price_euro = li.findAll('span', {'class': 'integer'})
+            product_price_cent = li.findAll('span', {'class': 'decimal'})
 
-            # <ul data-v-04484ba1="" class="listing-content__list listing-content__list--grid"> i want to get this element with soup
-            product_group = soup.select_one('.listing-content__list')
+            # Get product data
+            product_name = product_name_span[0].text.strip()
+            product_link = PRODUCT_URL + product_link_anchor[0]['href']
+            product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}'
 
-            # Find all li elements inside ul_element, excluding those with hidden="hidden"
-            product_list = product_group.find_all('li', attrs={'hidden': None})
+            parts = product_link.split('-')
+            product_id = parts[-1]
 
-            # print(li_elements)
+            product_info = {
+                'key': product_id,
+                'name': product_name,
+                'price': product_price,
+                'link': product_link
+            }
 
-            product_total = len(product_list)
+            product_dict[query][product_id] = product_info
 
-            # Process the li elements as needed
-            for li in product_list:
-
-                # Find product HTML Elements
-                product_name_span = li.findAll('span', {'class': 'produc-card__name__link'})
-                product_link_anchor = li.findAll('a')
-                product_price_euro = li.findAll('span', {'class': 'integer'})
-                product_price_cent = li.findAll('span', {'class': 'decimal'})
-
-                # Get product data
-                product_name = product_name_span[0].text.strip()
-                product_link = PRODUCT_URL + product_link_anchor[0]['href']
-                product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}'
-
-                parts = product_link.split('-')
-                product_id = parts[-1]
-
-                product_info = {
-                    'key': product_id,
-                    'name': product_name,
-                    'price': product_price,
-                    'link': product_link
-                }
-
-                product_dict[query][product_id] = product_info
-
-            save_products()
+        save_products()
+        return("NEXT_PAGE")
     else:
-        print("ACCESS_ERROR")
-        return "ACCESS_ERROR"
+
+        p_element = soup.find('p', {'class': 'filter-and-sortblock__product-count'})
+
+        if p_element is None:
+            return "ACCESS_ERROR"
+
+        else:
+            return ('EMPTY_PAGE')
+
+
 
 
 print("start")
@@ -143,18 +140,17 @@ bot = telegram.Bot(token=TOKEN)
 
 load_queries()
 #load_products()
-print(queries)
 
 for query in queries:
-    # Worten sometimes has a problem with cookies, this loop tries to access it until gets what's needed
-    while True:
-        print(query)
-        soup = runWebDriver(
-            f'https://www.worten.pt/search?query=outlet%20{query}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock')
-        if soup != "ACCESS_ERROR":
-            break
-    product_dict.setdefault(query, {})
-    getData(query, soup, bot)
+    page = 1
+    result = ""
+    while result != "EMPTY_PAGE" and result != "ACCESS_ERROR":
+        print(f'fetching all "{query}" - page {page}')
+        soup = runWebDriver(f'https://www.worten.pt/search?query=outlet%20{query}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock&page={page}')
+        product_dict.setdefault(query, {})
+        result = getData(query, soup, bot)  # Call getData function for each page
+        if result == "NEXT_PAGE":
+            page = page + 1
     #time.sleep(10)
 
 # Always running script
