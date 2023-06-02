1fec1f9e0146b5a5960c756bfeca98b2e4cc2160
Henrique Leote <henriqueleote@outlook.com>
2023-06-02 22:49:02 +0200

    Working, sending messages and loop fixed

--- WortenChannelBot.py
+++ WortenChannelBot.py
@@ -1,6 +1,9 @@
+import re
+
 from selenium.webdriver.common.by import By
 from undetected_chromedriver import Chrome, ChromeOptions
 # pip install -U git+https://github.com/ultrafunkamsterdam/undetected-chromedriver@fix-multiple-instance
+# Bot.send_message' was never awaited, Enable tracemalloc to get the object allocation traceback - pip install python-telegram-bot==13.13
 import json
 import time
 from bs4 import BeautifulSoup
@@ -14,11 +17,12 @@ added_product_list = {}
 final_product_dict = {}
 queries = []
 
+
 # Local storage file
 DATA_FILE = "data.json"
 
 # Telegram Bot Token
-TOKEN = '6201495078:AAGmPD9vEI_dIT1D4uAMbF2_9Rx3dOzc1Bg'
+TOKEN = '5849084397:AAGWlLjZIdO3Ize5Myl_gG5k7N3FV0PURmM'
 
 # Telegram Channel ID
 channel_id = "-1001921638321"
@@ -117,17 +121,41 @@ def getData(query, soup, bot):
         product_list = product_group.find_all('li', attrs={'hidden': None})
 
         # Process the li elements as needed
-        for li in product_list:
+        for product_li in product_list:
             # Find product HTML Elements
-            product_name_span = li.findAll('span', {'class': 'produc-card__name__link'})
-            product_link_anchor = li.findAll('a')
-            product_price_euro = li.findAll('span', {'class': 'integer'})
-            product_price_cent = li.findAll('span', {'class': 'decimal'})
+            img_src = ""
+            grade = ""
+            product_name_span = product_li.findAll('span', {'class': 'produc-card__name__link'})
+            product_link_anchor = product_li.findAll('a')
+            product_price_euro = product_li.findAll('span', {'class': 'integer'})
+            product_price_cent = product_li.findAll('span', {'class': 'decimal'})
+            img_element = product_li.findAll('img', {'class': 'product-card__image'})
+            gradePattern = r"Grade (A\+?|B|C)"
 
             # Get product data
             product_name = product_name_span[0].text.strip()
             product_link = PRODUCT_URL + product_link_anchor[0]['href']
             product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}'
+            grade_string = re.search(gradePattern, product_name)
+            img_src = img_element[0].get("src")
+            if grade_string is not None:
+                grade = grade_string.group(1)
+
+            if grade == "A+":
+                emoji = "\U0001F535"
+            elif grade == "A":
+                emoji = "\U0001F7E2"
+            elif grade == "B":
+                emoji = "\U0001F7E1"
+            elif grade == "C":
+                emoji = "\U0001F7E0"
+            else:
+                grade = "Outro"
+                emoji = "\u26AA"  # White circle emoji
+
+            # Emoji
+            red_circle = "\U0001F534"
+            white_circle = "\u26AA"
 
             parts = product_link.split('-')
             product_id = parts[-1]
@@ -137,13 +165,23 @@ def getData(query, soup, bot):
                 'name': product_name,
                 'price': product_price,
                 'link': product_link,
-                'query': query
+                'query': query,
+                'grade': grade
             }
 
             # Check if already exists in the existing list
             if product_id not in old_product_list[query]:
                 print('new product -> ' + product_name)
                 added_product_list[query][product_id] = product_info
+
+                # Send message to telegram
+                title = f'{red_circle}{white_circle} Worten {white_circle}{red_circle}\n'
+                message = f'{title}{product_name}\nPrice: {product_price}\nCondition: Grade {grade} {emoji}\n{product_link}'
+                if (img_src != ''):
+                    bot.send_photo(chat_id=channel_id, photo=img_src, caption=message)
+                else:
+                    bot.send_message(chat_id=channel_id, text=message)
+
 
             new_product_list[query][product_id] = product_info
 
@@ -159,15 +197,11 @@ def getData(query, soup, bot):
             return ('EMPTY_PAGE')
 
 
-
-
-print("start")
-
 # Start Bot with defined TOKEN
 bot = telegram.Bot(token=TOKEN)
 
 # Send a start-up message to the Chat
-# bot.send_message(chat_id=channel_id, text='Now getting updates from Worten')
+bot.send_message(chat_id=channel_id, text='Now getting updates from Worten')
 
 load_queries()
 load_products()
@@ -178,19 +212,24 @@ while True:
     for query in queries:
         result = ""
         page = 1
-        while result != "EMPTY_PAGE" and result != "ACCESS_ERROR":
+        while True:
             print(f'fetching all "{query}" - page {page}')
             soup = runWebDriver(f'https://www.worten.pt/search?query=outlet%20{query}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock&page={page}')
             old_product_list.setdefault(query, {})
             new_product_list.setdefault(query, {})
             added_product_list.setdefault(query, {})
             result = getData(query, soup, bot)  # Call getData function for each page
-            if result == "NEXT_PAGE":
-                page = page + 1
             if result == "EMPTY_PAGE":
-                break
+                print("moving to the next item")
+                break  # Exit the loop if an empty page is encountered
             if result == "ACCESS_ERROR":
-                break
+                # Retry accessing the page
+                print("access problem, trying to access again")
+                continue  # Continue to the next iteration of the loop
+            # Process the data or perform any other necessary actions
+            if result == "NEXT_PAGE":
+                print("moving to the next page")
+                page += 1  # Move to the next page
     compareLists()
     save_products()
     time.sleep(3600)
