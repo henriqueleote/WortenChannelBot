70b92b440d63d933b6ce58fd0274b36e7688a5a8
Henrique Leote <henriqueleote@outlook.com>
2023-06-02 10:23:15 +0200

    Getting products name, price and link

--- WortenChannelBot.py
+++ WortenChannelBot.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from undetected_chromedriver import Chrome, ChromeOptions
 #pip install -U git+https://github.com/ultrafunkamsterdam/undetected-chromedriver@fix-multiple-instance
 import json
@@ -7,7 +8,8 @@ import telegram
 
 # URL of the website you want to fetch
 search_category = 'tablet'
-URL = f'https://www.worten.pt/search?query=outlet%20{search_category}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock'
+QUERY_URL = f'https://www.worten.pt/search?query=outlet%20{search_category}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock'
+PRODUCT_URL = f'https://worten.pt'
 
 # Variables
 most_recent = None
@@ -54,11 +56,11 @@ def runWebDriver(category):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = Chrome(options=options)
-
-    driver.get(URL)
+    driver.get(QUERY_URL)
 
     # Retrive source code
     html_content = driver.page_source
+
 
     # Close driver
     driver.quit()
@@ -75,47 +77,44 @@ def getData(bot):
 
     p_element = soup.find('p', {'class': 'filter-and-sortblock__product-count'})
 
-    span_element = p_element.find('span')
+    if(p_element != None):
+        span_element = p_element.find('span')
 
-    product_count = span_element.contents[0]
+        product_count = span_element.contents[0]
 
-    if (int(product_count) > 0):
+        if (int(product_count) > 0):
 
-        #<ul data-v-04484ba1="" class="listing-content__list listing-content__list--grid"> i want to get this element with soup
-        ul_element = soup.select_one('.listing-content__list')
-
-        # Find all li elements inside ul_element, excluding those with hidden="hidden"
-        li_elements = ul_element.find_all('li', attrs={'hidden': None})
-
-        def getData(bot):
-            print('running...')
-
-            # Get internet code
-            soup = runWebDriver('tablet')
-
-            # Find the ul element with class "listing-content__list"
-            ul_element = soup.select_one('.listing-content__list')
+            # <ul data-v-04484ba1="" class="listing-content__list listing-content__list--grid"> i want to get this element with soup
+            product_group = soup.select_one('.listing-content__list')
 
             # Find all li elements inside ul_element, excluding those with hidden="hidden"
-            li_elements = ul_element.find_all('li', attrs={'hidden': None})
+            product_list = product_group.find_all('li', attrs={'hidden': None})
+
+            # print(li_elements)
 
             # Process the li elements as needed
-            for li in li_elements:
-                # Find the span element with class "product-card__name__link" inside each li
-                product_name_span = li.find('span', class_='product-card__name__link')
+            #for li in product_list:
+            # Find the span element with class "product-card__name__link" inside each li
+            product_name_span = product_list[0].findAll('span', {'class': 'produc-card__name__link'})
+            product_link_anchor = product_list[0].findAll('a')
+            product_price_euro = product_list[0].findAll('span', {'class': 'integer'})
+            product_price_cent = product_list[0].findAll('span', {'class': 'decimal'})
 
-                # Extract the text inside the span element
-                product_name = product_name_span.get_text(strip=True)
+            product_name = product_name_span[0].text
+            product_link = PRODUCT_URL + product_link_anchor[0]['href']
+            product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}€'
 
-                # Do something with the product name
-                print(product_name)
+            print(f'{product_name} -> {product_link} -> {product_price}')
 
-            # Rest of the code...
+            # Extract the text inside the span element
+            # product_name = product_name_span.get_text(strip=True)
 
-        # Process the li elements as needed
-        for li in li_elements:
-            # Do something with each li element
-            print(f'{li}\n')
+            # Do something with the product name
+    else:
+        print("cookie problem, re-run the program until in works.")
+
+
+
 
 
 # Start Bot with defined TOKEN
