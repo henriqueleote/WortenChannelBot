18242e82af1ca91cc1cb0de82665a45de0f3a727
Henrique Leote <henriqueleote@outlook.com>
2023-06-02 21:39:40 +0200

    System working, checking new products and removing the ones that doesnt exist anymore (hard to test))

--- WortenChannelBot.py
+++ WortenChannelBot.py
@@ -8,7 +8,10 @@ import telegram
 
 # URL of the website you want to fetch
 PRODUCT_URL = f'https://worten.pt'
-product_dict = {}
+old_product_list = {}
+new_product_list = {}
+added_product_list = {}
+final_product_dict = {}
 queries = []
 
 # Local storage file
@@ -39,26 +42,49 @@ def load_queries():
 
 # Load products from the JSON file
 def load_products():
-    global product_dict
+    global old_product_list
     try:
         with open(DATA_FILE, 'r') as file:
             data = file.read()
             if data:
-                product_dict = json.loads(data)
+                old_product_list = json.loads(data)
             else:
-                product_dict = {}
+                old_product_list = {}
     except FileNotFoundError:
-        product_dict = {}
+        old_product_list = {}
 
 
 # Save products to the JSON file
 def save_products():
-    global product_dict
+    global final_product_dict
+
+    print("saving lists")
+
     with open(DATA_FILE, 'w') as file:
-        if product_dict:
-            json.dump(product_dict, file)
+        if final_product_dict:
+            json.dump(final_product_dict, file)
         else:
             file.write('')
+
+
+def compareLists():
+    global old_product_list, new_product_list, added_product_list, final_product_dict
+
+    print("comparing lists")
+
+    for query, old_query_products in old_product_list.items():
+        new_query_products = new_product_list.get(query, {})
+        added_query_products = added_product_list.get(query, {})
+
+        for product_id in list(old_query_products.keys()):
+            if product_id not in new_query_products:
+                del old_query_products[product_id]
+
+        old_query_products.update(added_query_products)
+        final_product_dict[query] = old_query_products
+
+    return final_product_dict
+    # Now, final_product_list contains the merged list with the desired criteria
 
 
 def runWebDriver(link):
@@ -110,12 +136,17 @@ def getData(query, soup, bot):
                 'key': product_id,
                 'name': product_name,
                 'price': product_price,
-                'link': product_link
+                'link': product_link,
+                'query': query
             }
 
-            product_dict[query][product_id] = product_info
+            # Check if already exists in the existing list
+            if product_id not in old_product_list[query]:
+                print('new product -> ' + product_name)
+                added_product_list[query][product_id] = product_info
 
-        save_products()
+            new_product_list[query][product_id] = product_info
+
         return("NEXT_PAGE")
     else:
 
@@ -139,24 +170,27 @@ bot = telegram.Bot(token=TOKEN)
 # bot.send_message(chat_id=channel_id, text='Now getting updates from Worten')
 
 load_queries()
-#load_products()
-
-for query in queries:
-    page = 1
-    result = ""
-    while result != "EMPTY_PAGE" and result != "ACCESS_ERROR":
-        print(f'fetching all "{query}" - page {page}')
-        soup = runWebDriver(f'https://www.worten.pt/search?query=outlet%20{query}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock&page={page}')
-        product_dict.setdefault(query, {})
-        result = getData(query, soup, bot)  # Call getData function for each page
-        if result == "NEXT_PAGE":
-            page = page + 1
-    #time.sleep(10)
+load_products()
 
 # Always running script
-#while True:
+while True:
     # For all types of product in file queries.txt
-
-    #for query code here
-
-    #time.sleep(300)
+    for query in queries:
+        result = ""
+        page = 1
+        while result != "EMPTY_PAGE" and result != "ACCESS_ERROR":
+            print(f'fetching all "{query}" - page {page}')
+            soup = runWebDriver(f'https://www.worten.pt/search?query=outlet%20{query}&sort_by=price&order_by=asc&facetFilters=seller_id%3A689dda97-efa4-4c6d-96bc-6f4bbfda8394&facetFilters=t_tags%3Ais_in_stock&page={page}')
+            old_product_list.setdefault(query, {})
+            new_product_list.setdefault(query, {})
+            added_product_list.setdefault(query, {})
+            result = getData(query, soup, bot)  # Call getData function for each page
+            if result == "NEXT_PAGE":
+                page = page + 1
+            if result == "EMPTY_PAGE":
+                break
+            if result == "ACCESS_ERROR":
+                break
+    compareLists()
+    save_products()
+    time.sleep(3600)
