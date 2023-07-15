d4af7398724c3319ca44e7fc6d41f5ad5f312007
Henrique Leote <henriqueleote@outlook.com>
2023-07-15 21:04:32 +0200

    Code optimization by 60%

--- WortenChannelBot.py
+++ WortenChannelBot.py
@@ -74,7 +74,7 @@ def save_products():
 def compareLists():
     global old_list, recent_list, added_products_list, final_products_dict
 
-    print("comparing lists")
+    print("comparing lists...")
 
     merged_product_dict = {}
 
@@ -89,6 +89,8 @@ def compareLists():
 
     final_products_dict = merged_product_dict
 
+    print("lists compared")
+
     return final_products_dict
     # Now, final_product_dict contains the merged list with the desired structure
 
@@ -96,93 +98,89 @@ def compareLists():
 def runWebDriver(driver, link):
     driver.get(link)
 
-    # Retrive source code
-    html_content = driver.page_source
-
     # Parse and return HTML code
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     return soup
 
 
 def getData(soup, bot):
 
-    # <ul data-v-04484ba1="" class="listing-content__list listing-content__list--grid"> i want to get this element with soup
+    product_count_element = soup.find('p', class_='filter-and-sortblock__product-count')
+
+    print(product_count_element)
+
+    if product_count_element is not None:
+        product_count_text = product_count_element.get_text(strip=True)
+        product_count = re.search(r'\d+', product_count_text).group()
+        if int(product_count) >= 500:
+            return 'EMPTY_PAGE'
+
     product_group = soup.select_one('.listing-content__list')
 
     if product_group is not None:
 
         # Find all li elements inside ul_element, excluding those with hidden="hidden"
-        product_list = product_group.find_all('li', attrs={'hidden': None})
+        product_list = product_group.select('li:not([hidden])')
 
         # Process the li elements as needed
         for product_li in product_list:
-            # Find product HTML Elements
-            img_src = ""
-            grade = ""
-            product_discount = ""
-            product_name_span = product_li.findAll('span', {'class': 'produc-card__name__link'})
+
             product_link_anchor = product_li.findAll('a')
-            product_price_euro = product_li.findAll('span', {'class': 'integer'})
-            product_price_cent = product_li.findAll('span', {'class': 'decimal'})
-            img_element = product_li.findAll('img', {'class': 'product-card__image'})
-            gradePattern = r"Grade (A\+?|B|C)"
-
-            # Get product data
-            product_name = product_name_span[0].text.strip()
             product_link = PRODUCT_URL + product_link_anchor[0]['href']
-            product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}'
-            grade_string = re.search(gradePattern, product_name)
-            img_src = img_element[0].get("src")
-
-            if grade_string is not None:
-                grade = grade_string.group(0)
-
-            if grade == "Grade A+":
-                emoji = "\U0001F535"
-            elif grade == "Grade A":
-                emoji = "\U0001F7E2"
-            elif grade == "Grade B":
-                emoji = "\U0001F7E1"
-            elif grade == "Grade C":
-                emoji = "\U0001F7E0"
-            else:
-                grade = "Outro"
-                emoji = "\u26AA"  # White circle emoji
-
-            # Emoji
-            red_circle = "\U0001F534"
-            white_circle = "\u26AA"
-
             parts = product_link.split('-')
             product_id = parts[-1]
 
             product_info = {
-                'key': product_id,
-                'name': product_name,
-                'price': product_price,
-                'link': product_link,
-                'grade': grade
+                'key': product_id
             }
 
-            lowercase_string = product_name.lower()
-            if "grade" in lowercase_string or "reuse" in lowercase_string or "recondicionado" in lowercase_string or "caixa aberta" in lowercase_string:
-                product_discount_span = product_li.findAll('span', {'class': 'bold'})
-                if (product_discount_span):
-                    product_discount = re.sub("[^0-9]", "", product_discount_span[0].text.strip())
+            if product_id not in old_list and product_id not in added_products_list:
 
-                if product_discount and int(product_discount) >= MINIMUM_DISCOUNT:
-                    # Check if doesn't exist anywhere
-                    if product_id not in old_list and product_id not in added_products_list:
+                # Find product HTML Elements
+                img_src = ""
+                grade = ""
+                product_discount = ""
+                product_name_span = product_li.findAll('span', {'class': 'produc-card__name__link'})
+                product_price_euro = product_li.findAll('span', {'class': 'integer'})
+                product_price_cent = product_li.findAll('span', {'class': 'decimal'})
+                img_element = product_li.findAll('img', {'class': 'product-card__image'})
+                gradePattern = r"Grade (A\+?|B|C)"
+
+                # Get product data
+                product_name = product_name_span[0].text.strip()
+
+                product_price = f'{product_price_euro[0].text},{product_price_cent[0].text}'
+                grade_string = re.search(gradePattern, product_name)
+                img_src = img_element[0].get("src")
+
+                grade_emoji_map = {
+                    "Grade A+": "\U0001F535",
+                    "Grade A": "\U0001F7E2",
+                    "Grade B": "\U0001F7E1",
+                    "Grade C": "\U0001F7E0"
+                }
+
+                grade = grade_string.group(0) if grade_string else "Outro"
+                emoji = grade_emoji_map.get(grade, "\u26AA")  # White circle emoji
+
+                lowercase_string = product_name.lower()
+                if "grade" in lowercase_string or "reuse" in lowercase_string or "recondicionado" in lowercase_string or "caixa aberta" in lowercase_string:
+                    product_discount_span = product_li.findAll('span', {'class': 'bold'})
+                    if (product_discount_span):
+                        product_discount = re.sub("[^0-9]", "", product_discount_span[0].text.strip())
+
+                    if product_discount and int(product_discount) >= MINIMUM_DISCOUNT:
+                        # Check if doesn't exist anywhere
                         print('new product -> ' + product_name)
                         added_products_list[product_id] = product_info
 
                         # Send message to telegram
-                        title = f'{red_circle}{white_circle} Worten {white_circle}{red_circle}\n'
-                        message = f'{title}{product_name}\nPrice: {product_price}€\nCondition: {grade} {emoji}\nDiscount: {product_discount}%\n{product_link}'
+                        title = f'\U0001F534\u26AA Worten \u26AA\U0001F534\n'
+                        message = f'{title}{product_name}\nPrice: {product_price}€\nCondition: {emoji} {grade} \nDiscount: {product_discount}%\n{product_link}'
                         if (img_src != ''):
                             bot.send_photo(chat_id=channel_id, photo=img_src, caption=message)
                             time.sleep(3)
-                    recent_list[product_id] = product_info
+            recent_list[product_id] = product_info
 
         return("NEXT_PAGE")
     else:
@@ -199,23 +197,21 @@ def getData(soup, bot):
 # Start Bot with defined TOKEN
 bot = telegram.Bot(token=TOKEN)
 
-# Send a start-up message to the Chat
-#bot.send_message(chat_id=channel_id, text='Now getting updates from Worten every hour')
-
 load_queries()
 load_products()
 
-options = ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-
-# Start driver
-driver = Chrome(options=options)
-
 # Always running script
 while True:
+
+    options = ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    # Start driver
+    driver = Chrome(options=options)
+
     # For all types of product in file queries.txt
     for query in queries:
         result = ""
@@ -238,7 +234,8 @@ while True:
     compareLists()
     save_products()
 
+    # Close driver
+    driver.quit()
+
     time.sleep(INTERVAL)
 
-# Close driver
-driver.quit()
