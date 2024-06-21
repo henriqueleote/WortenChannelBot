3b74f13093853d72a2d18587fa51a671e6cb239c
Henrique Leote <henriqueleote@outlook.com>
2024-06-21 12:48:18 +0200

    Error handling

--- bot.py
+++ bot.py
@@ -5,11 +5,10 @@ import telegram
 from telegram.error import BadRequest, RetryAfter, TimedOut, NetworkError
 import worten_config
 
-#queries = ['apple','samsung','tablet','consola','portatil','huawei','smart tv','smartphone','drone']
+# queries = ['apple','samsung','tablet','consola','portatil','huawei','smart tv','smartphone','drone']
 queries = ['outlet']
 
 list = []
-
 
 iteration = 0
 
@@ -23,11 +22,69 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
 }
 
+
+async def handleItem(item):
+    productID = item['product']['sku']
+    productName = item['product']['name']
+    productImage = item['product']['image']['url']
+    productGrade = item['product']['textProperties'].get('grade-recon', {})
+    productQuantity = item['totalOffers']
+
+    if not productGrade:
+        productGrade = 'Últimas unidades'
+    productFinalPrice = str(item['winningOffer']['pricing']['final']['value'])
+    formatedProductFinalPrice = float(f"{productFinalPrice[:-2]}.{productFinalPrice[-2:]}")
+
+    url = f'https://worten.pt{item["product"]["url"]}'
+
+    grade_emoji_map = {
+        "A+": f"\U0001F535 Grade",
+        "A": f"\U0001F7E2 Grade",
+        "B": f"\U0001F7E1 Grade",
+        "C": f"\U0001F7E0 Grade"
+    }
+
+    emoji = grade_emoji_map.get(productGrade, "\u26AA")  # White circle emoji
+
+    title = f'\U0001F534\u26AA Worten \u26AA\U0001F534\n'
+    message = (f'{title}{productName}\n'
+               f'Price: {formatedProductFinalPrice}€\n'
+               f'Condition: {emoji} {productGrade}\n'
+               f'Quantity: {productQuantity}\n'
+               f'{url}')
+
+    if (productID not in list):
+        list.append(productID)
+        print(f'new product -> {formatedProductFinalPrice}€ | {productName}')
+        if sendMessage and iteration > 0:
+            try:
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+            except BadRequest as e:
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+            except RetryAfter as e:
+                time.sleep(e.retry_after)
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+            except TimedOut as e:
+                time.sleep(60)
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+            except NetworkError as e:
+                time.sleep(30)
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+            except Exception as e:
+                time.sleep(1)
+                async with bot:
+                    await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
+
+
 async def queryWebsite(query):
-    moveToTheNextOne = False
     page = 0
 
-    while moveToTheNextOne != True:
+    while True:
         json_data = {
             'variables': {
                 'query': 'outlet ' + query,
@@ -74,82 +131,27 @@ async def queryWebsite(query):
                 },
             },
         }
-
         response = requests.post('https://www.worten.pt/_/api/graphql', headers=headers, json=json_data)
-        if response:
-            data = response.json().get('data',[]).get('searchProducts',[]).get('hits',[])
+        if response.json():
+            mainSet = response.json().get('data', [])
+            if mainSet:
+                searchProducts = mainSet.get('searchProducts', [])
+                if searchProducts:
+                    hasNextPage = searchProducts.get('hasNextPage', False)
+                    data = searchProducts.get('hits', [])
+                    # print(hasNextPage)
+                    # print(data is not None)
+                    if data:
+                        for item in data:
+                            await handleItem(item)
+                    if hasNextPage:
+                        page += 1
+                    else:
+                        break
 
-            try:
-                hasNextPage = response.json()['data']['searchProducts']['hasNextPage']
-            except KeyError:
-                hasNextPage = False
-                moveToTheNextOne = False
-                print('no record')
-
-            for item in data:
-                productID = item['product']['sku']
-                productName = item['product']['name']
-                productImage = item['product']['image']['url']
-                productGrade = item['product']['textProperties'].get('grade-recon', {})
-                productQuantity = item['totalOffers']
-
-                if not productGrade:
-                    productGrade = 'Últimas unidades'
-                productFinalPrice = str(item['winningOffer']['pricing']['final']['value'])
-                formatedProductFinalPrice = float(f"{productFinalPrice[:-2]}.{productFinalPrice[-2:]}")
-
-                url = f'https://worten.pt{item["product"]["url"]}'
-
-                grade_emoji_map = {
-                    "A+": f"\U0001F535 Grade",
-                    "A": f"\U0001F7E2 Grade",
-                    "B": f"\U0001F7E1 Grade",
-                    "C": f"\U0001F7E0 Grade"
-                }
-
-                emoji = grade_emoji_map.get(productGrade, "\u26AA")  # White circle emoji
-
-                title = f'\U0001F534\u26AA Worten \u26AA\U0001F534\n'
-                message = (f'{title}{productName}\n'
-                           f'Price: {formatedProductFinalPrice}€\n'
-                           f'Condition: {emoji} {productGrade}\n'
-                           f'Quantity: {productQuantity}\n'
-                           f'{url}')
-
-                if (productID not in list):
-                    list.append(productID)
-                    print(f'new product -> {formatedProductFinalPrice}€ | {productName}')
-                    if sendMessage and iteration > 0:
-                        try:
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-                        except BadRequest as e:
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-                        except RetryAfter as e:
-                            time.sleep(e.retry_after)
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-                        except TimedOut as e:
-                            time.sleep(60)
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-                        except NetworkError as e:
-                            time.sleep(30)
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-                        except Exception as e:
-                            time.sleep(1)
-                            async with bot:
-                                await bot.send_photo(chat_id=channel_id, photo=productImage, caption=message)
-
-            if hasNextPage:
-                page += 1
-            else:
-                moveToTheNextOne = True
-                break
 
 bot = telegram.Bot(token=worten_config.TOKEN)
+
 
 async def main():
     global iteration
@@ -163,6 +165,7 @@ async def main():
                                    text=f'Last worten product -> {list[len(list) - 1]}', disable_notification=True)
         iteration += 1
         time.sleep(180)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
